72eec3c00bf26f59b0caa1a4177e75d7f6f90c4f
Jared Padilla <jaredpadilla16@gmail.com>
2016-04-19 00:19:01 -0600

    Added Purpose, Audience, and Goals.

--- public_html/documentation/mileston-1.php
+++ public_html/documentation/mileston-1.php
@@ -1,7 +1,12 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: JPadilla16
- * Date: 4/18/16
- * Time: 11:03 PM
- */
+Purpose, Audience, and Goal
+
+Purpose: This Project is professional portfolio with a focus on web development.
+
+Audience: I will be designing the site with a professional audience in mind, targetting
+individuals in the web development, I.T., as well as for individuals who are looking for web
+development.
+
+Goal: The Primary goal of my site is for users of the site to contact me for web development
+opportunities through connected social media or contact form. Secondary goal of site is to display
+all my work and present a professional online presence.
+
