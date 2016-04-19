88a82b0fa3dd2a72ba0cf8f99a6c24954bf778d2
Jared Padilla <jaredpadilla16@gmail.com>
2016-04-19 02:54:43 -0600

    Eddited code to follow stlye guide

--- public_html/documentation/mileston-1.php
+++ public_html/documentation/mileston-1.php
@@ -1,46 +1,61 @@
-Purpose, Audience, and Goal
+<!Doctype html>
+<html lang="en"
+	<head>
+		<meta charset="ufc-8">
+		<title>jaredpadilla.com</title>
+	</head>
+	<body>
+		<header>
+			<h1>Jared Padilla</h1>
+		</header>
+		<main>
+			<p><h3>Purpose, Audience, and Goal</h3>
 
-Purpose: This Project is professional portfolio with a focus on web development.
+			Purpose: This Project is professional portfolio with a focus on web development.
 
-Audience: I will be designing the site with a professional audience in mind, targetting
-individuals in the web development, I.T., as well as for individuals who are looking for web
-development.
+			Audience: I will be designing the site with a professional audience in mind, targetting
+			individuals in the web development, I.T., as well as for individuals who are looking for web
+			development.
 
-Goal: The Primary goal of my site is for users of the site to contact me for web development
-opportunities through connected social media or contact form. Secondary goal of site is to display
-all my work and present a professional online presence.
+			Goal: The Primary goal of my site is for users of the site to contact me for web development
+			opportunities through connected social media or contact form. Secondary goal of site is to display
+			all my work and present a professional online presence.
 
-Persona
+			<h3>Persona</h3>
 
-Name:Chance Smith
+			Name:Chance Smith
 
-Age: 24 years old
+			Age: 24 years old
 
-Profession: Founder of CraftBeer.com, a startup that is looking to hire a full/part time Web
-Developer. They just recieved all their shipments from various craft breweries. They sell craft
-beers from all over the United States. 
+			Profession: Founder of CraftBeer.com, a startup that is looking to hire a full/part time Web
+			Developer. They just recieved all their shipments from various craft breweries. They sell craft
+			beers from all over the United States.
 
-Technology: Mac User. Uses a 13" Macbook pro laptop and has an iPhone 5c with unlimited 4GLTE.
+			Technology: Mac User. Uses a 13" Macbook pro laptop and has an iPhone 5c with unlimited 4GLTE.
 
-Attitudes and Behaviors: Short on time. Always has a full schedule. Although he is always busy 
-he likes to do thorough research on who he hires. He's not big on social media but will step out 
-of his comfort zone to do a little extra research on potential employees.
+			Attitudes and Behaviors: Short on time. Always has a full schedule. Although he is always busy
+			he likes to do thorough research on who he hires. He's not big on social media but will step out
+			of his comfort zone to do a little extra research on potential employees.
 
-Frustrations and Needs: Chance needs to hire someone who is going to care about the website like
-it was their own company website. He is looking someone with good ethics. He must hire as soon as
-possible so he can't start moving some beer.
+			Frustrations and Needs: Chance needs to hire someone who is going to care about the website like
+			it was their own company website. He is looking someone with good ethics. He must hire as soon as
+			possible so he can't start moving some beer.
 
-Goals: Chance is concerned with one thing, and that's someone who takes pride in their work.
+			Goals: Chance is concerned with one thing, and that's someone who takes pride in their work.
 
-Use Case: Chance recieves a cover letter from myself in regards to his Web Dev position for his 
-craft beer website. Based on my resume he considers me as one of the canidates to search for via 
-Linkedin.
+			Use Case: Chance recieves a cover letter from myself in regards to his Web Dev position for his
+			craft beer website. Based on my resume he considers me as one of the canidates to search for via
+			Linkedin.
 
-Being inexperienced he doesn't find much on linkedin but is still interested and wants to know 
-more so goes further and checks my personal website for more information.
+			Being inexperienced he doesn't find much on linkedin but is still interested and wants to know
+			more so goes further and checks my personal website for more information.
 
-Satisfied with my previous work on my personal website he takes the time to squeeze me in his
-busy schedule along with several of his other canidates. 
-
-
-
+			Satisfied with my previous work on my personal website he takes the time to squeeze me in his
+			busy schedule along with several of his other canidates.
+			</p>
+		</main>
+		<footer>
+			<div>&copy; 2016 JaredPadilla </div>
+		</footer>
+	</body>
+</html>
