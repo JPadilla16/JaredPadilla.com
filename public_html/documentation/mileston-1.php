8f89d902fc43796497d7c38ddfef8eb8d684f837
Jared Padilla <jaredpadilla16@gmail.com>
2016-05-01 22:01:16 -0600

    Made adjustments from milestone-1

--- public_html/documentation/mileston-1.php
+++ public_html/documentation/mileston-1.php
@@ -1,7 +1,7 @@
 <!Doctype html>
-<html lang="en"
+<html lang="en">
 	<head>
-		<meta charset="ufc-8">
+		<meta charset="utf-8">
 		<title>jaredpadilla.com</title>
 	</head>
 	<body>
@@ -11,48 +11,47 @@
 		<main>
 			<p><h3>Purpose, Audience, and Goal</h3>
 
-			Purpose: This Project is professional portfolio with a focus on web development.
+			<p>Purpose: This Project is professional portfolio with a focus on web development.</p>
 
-			Audience: I will be designing the site with a professional audience in mind, targetting
+			<p>Audience: I will be designing the site with a professional audience in mind, targetting
 			individuals in the web development, I.T., as well as for individuals who are looking for web
-			development.
+			development.</p>
 
-			Goal: The Primary goal of my site is for users of the site to contact me for web development
+			<p>Goal: The Primary goal of my site is for users of the site to contact me for web development
 			opportunities through connected social media or contact form. Secondary goal of site is to display
-			all my work and present a professional online presence.
+			all my work and present a professional online presence.</p>
 
 			<h3>Persona</h3>
 
-			Name:Chance Smith
+			<p>Name:Chance Smith</p>
 
-			Age: 24 years old
+			<p>Age: 24 years old</p>
 
-			Profession: Founder of CraftBeer.com, a startup that is looking to hire a full/part time Web
+			<p>Profession: Founder of CraftBeer.com, a startup that is looking to hire a full/part time Web
 			Developer. They just recieved all their shipments from various craft breweries. They sell craft
-			beers from all over the United States.
+			beers from all over the United States.</p>
 
-			Technology: Mac User. Uses a 13" Macbook pro laptop and has an iPhone 5c with unlimited 4GLTE.
+			<p>Technology: Mac User. Uses a 13" Macbook pro laptop and has an iPhone 5c with unlimited 4GLTE.</p>
 
-			Attitudes and Behaviors: Short on time. Always has a full schedule. Although he is always busy
+			<p>Attitudes and Behaviors: Short on time. Always has a full schedule. Although he is always busy
 			he likes to do thorough research on who he hires. He's not big on social media but will step out
-			of his comfort zone to do a little extra research on potential employees.
+			of his comfort zone to do a little extra research on potential employees.</p>
 
-			Frustrations and Needs: Chance needs to hire someone who is going to care about the website like
+			<p>Frustrations and Needs: Chance needs to hire someone who is going to care about the website like
 			it was their own company website. He is looking someone with good ethics. He must hire as soon as
-			possible so he can't start moving some beer.
+			possible so he can't start moving some beer.</p>
 
-			Goals: Chance is concerned with one thing, and that's someone who takes pride in their work.
+			<p>Goals: Chance is concerned with one thing, and that's someone who takes pride in their work.</p>
 
-			Use Case: Chance recieves a cover letter from myself in regards to his Web Dev position for his
+			<p>Use Case: Chance recieves a cover letter from myself in regards to his Web Dev position for his
 			craft beer website. Based on my resume he considers me as one of the canidates to search for via
-			Linkedin.
+			Linkedin.</p>
 
-			Being inexperienced he doesn't find much on linkedin but is still interested and wants to know
-			more so goes further and checks my personal website for more information.
+			<p>Being inexperienced he doesn't find much on linkedin but is still interested and wants to know
+			more so goes further and checks my personal website for more information.</p>
 
-			Satisfied with my previous work on my personal website he takes the time to squeeze me in his
-			busy schedule along with several of his other canidates.
-			</p>
+			<p>Satisfied with my previous work on my personal website he takes the time to squeeze me in his
+			busy schedule along with several of his other canidates.</p>
 		</main>
 		<footer>
 			<div>&copy; 2016 JaredPadilla </div>
